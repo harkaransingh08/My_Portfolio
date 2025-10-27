7c98db01a82d01b1b7da1e9e1df742ba5038f095
harkaransingh08 <harkaransingh08ktl@gmail.com>
2025-10-27 22:30:14 +0530

    hi

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className='flex justify-evenly py-20'>
 
     <ul className='max-w-xl text-justify leading-relaxed text-white font-medium arial text-lg text-balance'>
-      <p>I’m Monkey, a curious mind trapped in a suit, driven by instinct and imagination. I believe in solving complex problems with simple ideas, learning fast, and adapting faster. Whether it’s coding, creating, or just figuring out how the world works, I approach every challenge with a mix of logic, curiosity, and a bit of wild energy. Intelligence may have evolved, but curiosity never left the jungle.</p>
+      <p>Im Monkey, a curious mind trapped in a suit, driven by instinct and imagination. I believe in solving complex problems with simple ideas, learning fast, and adapting faster. Whether it’s coding, creating, or just figuring out how the world works, I approach every challenge with a mix of logic, curiosity, and a bit of wild energy. Intelligence may have evolved, but curiosity never left the jungle.</p>
     </ul>
 
     <ul >
