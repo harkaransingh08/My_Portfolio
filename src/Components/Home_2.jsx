90748a34376baee8889aeca184d3cdd658fa372e
Harkaran singh <harkaransingh08ktl@gmail.com>
2025-10-31 21:02:05 +0530

    21:01

--- src/Components/Home_2.jsx
+++ src/Components/Home_2.jsx
@@ -16,7 +16,7 @@ export default function Home_2() {
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
-      ><h1 className="flex justify-center items-center font-bold text-4xl hover:underline transition-all duration-300 text-white cursor-pointer hover:text-yellow-400  bg-white/10 backdrop-blur-md px-10 py-3 rounded-full m-10 ml-90 mr-90"
+      ><h1 className="flex justify-center items-center font-bold  text-4xl hover:underline transition-all duration-300 text-white cursor-pointer hover:text-yellow-400  bg-white/10 backdrop-blur-md px-10 py-3 rounded-full m-10 ml-60 mr-60"
         style={{ fontFamily: "'Oswald', sans-serif" }}>
           The Languages I Learned
         </h1>
